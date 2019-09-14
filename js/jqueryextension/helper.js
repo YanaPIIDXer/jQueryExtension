2d461f295827d113a70daab7ca252dedc916ad01
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-14 12:00:11 +0900

    上部から出ているかどうかも判定するようにした。

--- js/jqueryextension/helper.js
+++ js/jqueryextension/helper.js
@@ -1,7 +1,12 @@
 var is_inside_screen = function($obj, offset)
-{
+{   
     var $window = $(window);
+    
     var top = $obj.offset().top;
-    var border = $window.scrollTop() + $window.height();
-    return (border > top + offset);
+    var border_bottom = $window.scrollTop() + $window.height();
+
+    var bottom = top + $obj.height();
+    var border_top = $window.scrollTop();
+    
+    return (border_bottom > top + offset && border_top < bottom - offset);
 }
