b266fda6c05bc78f9f932b08f3994cbd5f210180
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-15 01:02:34 +0900

    カルーセルのjQuery拡張。

--- js/jqueryextension/attachtop.js
+++ js/jqueryextension/attachtop.js
@@ -1,4 +1,3 @@
-    
 $.fn.attachTop = function()
 {
     this.each(function()
---
+++ js/jqueryextension/carousel.js
@@ -0,0 +1,44 @@
+$.fn.setCarousel = function(changeTime, onActive, onDeactive)
+{
+    if(onActive == undefined)
+    {
+        onActive = function() {}
+    }
+    if(onDeactive == undefined)
+    {
+        onDeactive = function() {}
+    }
+    
+    var $items = [];
+    this.each(function()
+    {
+        $items.push($(this));
+        $(this).css({opacity: 0, position: "absolute"});
+        $(this).css("top", $items[0].css("top"));
+        $(this).bind("onCarouselActive", onActive);
+        $(this).bind("onCarouselDeactive", onDeactive);
+    });
+    if($items.length == 0) { return; }
+
+    $items[0].css("opacity", 1);
+    var currentIndex = 0;
+    function switchItem()
+    {
+        nextIndex = currentIndex + 1;
+        if(nextIndex >= $items.length)
+        {
+            nextIndex = 0;
+        }
+        $items[currentIndex].stop(true).animate({opacity: 0}, 500, "linear", function()
+        {
+            $(this).triggerHandler("onCarouselDeactive");
+        });
+        $items[nextIndex].stop(true).animate({opacity: 1}, 500, "linear");
+        currentIndex = nextIndex;
+        $items[currentIndex].triggerHandler("onCarouselActive");
+        setTimeout(switchItem, changeTime);
+    }
+
+    $items[0].triggerHandler("onCarouselActive");
+    setTimeout(switchItem, changeTime);
+}
