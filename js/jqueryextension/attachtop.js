b8a004e62790679f74f1076d7773b5ab48d8a1e4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-14 12:00:26 +0900

    画面のスクロールに追従させるjQuery拡張。

---
+++ js/jqueryextension/attachtop.js
@@ -0,0 +1,26 @@
+    
+$.fn.attachTop = function()
+{
+    this.each(function()
+    {
+        $self = $(this);
+        $self.css("position", "absolute");
+        defaultTop = $self.offset().top;
+        
+        $(window).scroll(function()
+        {
+            if($(window).scrollTop() > defaultTop)
+            {
+                $self.css("position", "fixed");
+                $self.css("top", 0);   
+            }
+            else
+            {
+                $self.css("position", "absolute");
+                $self.css("top", "auto");
+            }
+        });
+    });
+
+    $(window).scroll();
+}
