06c96fc8ad544a4bc2f0d711d9e31424e1b1a65b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-15 17:21:51 +0900

    終了コールバックを実装。

--- js/jqueryextension/delayfadein.js
+++ js/jqueryextension/delayfadein.js
@@ -1,17 +1,28 @@
 jQuery(function()
 {
-    $.fn.delayFadeIn = function(delayTime)
+    $.fn.delayFadeIn = function(delayTime, onFinished)
     {
+        if(onFinished == undefined)
+        {
+            onFinished = function() {}
+        }
+        
         $items = [$(this)];
         $items.push($(this).children());
         
         delay = 0;
-        $items.map(function($item)
+        $items.map(function($item, index)
         {
             $item.css("opacity", 0);
             setTimeout(function()
             {
-                $item.stop(true).animate({opacity: 1}, delay + delayTime, "linear");
+                $item.stop(true).animate({opacity: 1}, delay + delayTime, "linear", function()
+                {
+                    if(index == $items.length - 1)
+                    {
+                        onFinished();
+                    }
+                });
             }, delay);
             delay += delayTime;
         });
