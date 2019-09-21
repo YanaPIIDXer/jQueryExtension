ceae7b78a2303a18d8d8803931cb656db2588a52
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-22 07:38:41 +0900

    画面内に入った時に発火させるイベントメソッド追加。

--- js/jqueryextension/helper.js
+++ js/jqueryextension/helper.js
@@ -16,6 +16,40 @@ var is_inside_screen = function($obj, offset, defaultTop)
     return (border_bottom > top + offset && border_top < bottom - offset);
 }
 
+// 画面内に入った時に発火させるイベント
+jQuery(function()
+{
+    $.fn.onScreen = function(callback, offset)
+    {
+        if(offset == undefined)
+        {
+            offset = 0;
+        }
+
+        $(this).on("onscreen", callback);
+
+        var is_inside = false;
+        $self = $(this);
+        $(window).scroll(function()
+        {
+            if(is_inside_screen($self, offset))
+            {
+                if(!is_inside)
+                {
+                    $self.triggerHandler("onscreen");
+                    is_inside = true;
+                }
+            }
+            else
+            {
+                is_inside = false;
+            }
+        });
+
+        return this;
+    }
+});
+
 // 文字列の領域を返す。
 var check_str_area = function($obj)
 {
